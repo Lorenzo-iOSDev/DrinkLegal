02c67a3a8cc00b03f25f76b9dd6f416f63d37fda
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2021-06-01 14:57:37 +1200

    Initial Commit

---
+++ DrinkLegal/DrinkLegalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkLegalApp.swift
+//  DrinkLegal
+//
+//  Created by Lorenzo Zemp on 1/06/21.
+//
+
+import SwiftUI
+
+@main
+struct DrinkLegalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
