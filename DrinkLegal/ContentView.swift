bfc8130bc413170886e37b71f619527334a8858c
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2021-06-02 22:10:02 +1200

    General design laid out.

--- DrinkLegal/ContentView.swift
+++ DrinkLegal/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var birthDate = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.systemBackground)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .center) {
+                TextField("DD/MM/YYYY", text: $birthDate)
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .keyboardType(.numbersAndPunctuation)
+                    .padding(.horizontal, 100)
+                
+                Text("Enter Birthdate")
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .padding()
+                
+                
+            }
+        }
+        .overlay(Image(systemName: "arrow.uturn.backward.circle")
+                    .imageScale(.large)
+                    .frame(width: 44, height: 44)
+                    .padding(),
+                 alignment: .topTrailing)
+    }
+    
+    func checkTextFieldLength() {
+        
     }
 }
 
