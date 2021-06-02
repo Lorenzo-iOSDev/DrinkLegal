76c983743724261bfdb1d0742b094fadbe6217db
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2021-06-02 23:32:06 +1200

    changed keyboard type to number-pad style. should prevent user from inputting text.

--- DrinkLegal/ContentView.swift
+++ DrinkLegal/ContentView.swift
@@ -30,7 +30,7 @@ struct ContentView: View {
                         }
                     })
                     .multilineTextAlignment(.center)
-                    .keyboardType(.numbersAndPunctuation)
+                    .keyboardType(.numberPad)
                     .padding(.horizontal, 100)
                     
                 
