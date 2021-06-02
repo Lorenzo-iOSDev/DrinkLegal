7bfaba7427372a1e7e538cd0e0f5c9d4b43b210b
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2021-06-02 23:05:05 +1200

    Added automatic forward slash adding and string length limiter.

--- DrinkLegal/ContentView.swift
+++ DrinkLegal/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var birthDate = ""
+    let characterLimit = 10
     
     var body: some View {
         ZStack {
@@ -19,9 +20,19 @@ struct ContentView: View {
             VStack(alignment: .center) {
                 TextField("DD/MM/YYYY", text: $birthDate)
                     .font(.largeTitle)
+                    .onChange(of: birthDate, perform: { dateString in
+                        print("Text field count is: \(dateString.count)")
+                        
+                        if dateString.count == 2 || dateString.count == 5 {
+                            birthDate.append("/")
+                        } else if dateString.count > characterLimit {
+                            birthDate = String(birthDate.prefix(characterLimit))
+                        }
+                    })
                     .multilineTextAlignment(.center)
                     .keyboardType(.numbersAndPunctuation)
                     .padding(.horizontal, 100)
+                    
                 
                 Text("Enter Birthdate")
                     .font(.title)
@@ -36,10 +47,6 @@ struct ContentView: View {
                     .frame(width: 44, height: 44)
                     .padding(),
                  alignment: .topTrailing)
-    }
-    
-    func checkTextFieldLength() {
-        
     }
 }
 
